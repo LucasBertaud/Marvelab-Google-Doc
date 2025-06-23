57ac6afbaf710e8ad06f9cf31c364bd62140d31d
Lucas Bertaud <l.bertaud@hotmail.com>
2025-06-23 16:56:49 +0200

    feat(ai): add project context when generating a response with gemini.

--- api/src/ai/ai.controller.ts
+++ api/src/ai/ai.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { AiService } from './ai.service';
 import { GenerateResponseDto } from './dto/generate-response.dto';
 import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
@@ -8,7 +8,7 @@ import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 export class AiController {
   constructor(private readonly aiService: AiService) {}
 
-  @Post('generate-response')
+  @Post('generate-response/:id')
   @ApiOperation({ summary: 'Generate a response from AI' })
   @ApiBody({
     type: GenerateResponseDto,
@@ -21,11 +21,10 @@ export class AiController {
   })
   @ApiResponse({ status: 400, description: 'Invalid input data' })
   @ApiResponse({ status: 500, description: 'Internal server error' })
-  async generateResponse(@Body() dto: GenerateResponseDto): Promise<string> {
-    const trimPrompt = dto.prompt.trim();
-    if (trimPrompt === '') {
-      throw new Error('Prompt cannot be empty');
-    }
-    return this.aiService.generateResponse(trimPrompt);
+  async generateResponse(
+    @Body() dto: GenerateResponseDto,
+    @Param('id') id: string,
+  ): Promise<string> {
+    return this.aiService.generateResponse(dto.prompt, id);
   }
 }
--- api/src/infrastructure/ai/gemini/gemini.adapter.ts
+++ api/src/infrastructure/ai/gemini/gemini.adapter.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { AiPort } from '../../../ai/ai.port';
 import { GoogleGenAI } from '@google/genai';
+import { Project } from '../../../projects/entities/project.entity';
+import { Interpretation } from '../../../interpretations/entities/interpretation.entity';
+import { Note } from '../../../notes/entities/note.entity';
 
 @Injectable()
 export class GeminiAdapter implements AiPort {
   private ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
-  public async generateResponse(prompt: string): Promise<string> {
+  public async generateResponse(
+    prompt: string,
+    context: Project,
+  ): Promise<string> {
+    const contextualPrompt = await this.getContextualPrompt(context);
     const result = await this.ai.models.generateContent({
       model: 'gemini-2.0-flash',
-      contents: prompt,
+      contents: [
+        'Contexte du projet : ' + contextualPrompt,
+        "Prompt de l'utilisateur : " + prompt,
+      ],
     });
     const response = result.text.trim();
 
     return response;
+  }
+
+  private async getContextualPrompt(project: Project): Promise<string> {
+    return (
+      `Nom du projet : ${project.title}\n` +
+      `Description du projet : ${project.description}\n` +
+      `Notes : ${project.notes.map((note: Note, index: number) => `${index + 1} : ${note.content}`).join('\n')}\n` +
+      `Interprétations : ${project.interpretations.map((interpretation: Interpretation, index: number) => `${index + 1} : ${interpretation.content}\n`).join('\n')}\n`
+    );
   }
 }
